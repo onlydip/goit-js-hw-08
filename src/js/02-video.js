184d5ae60570dd0975c329c9d0da94ced0044f3b
Aleksandr Harianin <onlydipro@gmail.com>
2023-07-10 18:22:12 +0300

    add 2 imports and complete task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,17 @@
 
 import Player from '@vimeo/player';
-console.log(Player)
+// console.log(Player)
+
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(playerTimeUpdate, 1000));
+
+function playerTimeUpdate(data) {
+  const currentTime = data.seconds;
+
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
