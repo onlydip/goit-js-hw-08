6b895137951daaf2405f2ee85aed2ffdae704442
Aleksandr Harianin <onlydipro@gmail.com>
2023-07-10 18:03:49 +0300

    add npm gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+
+const listItems = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+      <img
+         class="gallery__image"
+         src="${preview}"
+         alt="${description}"
+    />
+  </a>`
+  )
+  .join('');
+
+listItems.insertAdjacentHTML('beforeend', galleryMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+// console.log(galleryItems);
